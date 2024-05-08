56ebf7f4697314c1f6001ce834c886f9101efff5
Igor Pissolati <igo08an@hotmail.com>
2024-05-08 20:57:35 -0300

    Add CORS heades to webserver

--- client-files/webserver.py
+++ client-files/webserver.py
@@ -3,16 +3,30 @@ from twisted.web.resource import Resource
 from twisted.internet import reactor, endpoints
 from twisted.web.static import File
 
+class CORSFile(File):
+    def render_GET(self, request):
+        request.setHeader(b"Access-Control-Allow-Origin", b"*")
+        request.setHeader(b"Access-Control-Allow-Methods", b"GET, POST, OPTIONS")
+        request.setHeader(b"Access-Control-Allow-Headers", b"Content-Type")
+        return File.render_GET(self, request)
+
+    def render_OPTIONS(self, request):
+        request.setHeader(b"Access-Control-Allow-Origin", b"*")
+        request.setHeader(b"Access-Control-Allow-Methods", b"GET, POST, OPTIONS")
+        request.setHeader(b"Access-Control-Allow-Headers", b"Content-Type")
+        request.setResponseCode(200)
+        return b""
+
 root = Resource()
-root.putChild(b"game", File("./www.godfield.net/game"))
-root.putChild(b"scripts", File("./www.godfield.net/scripts"))
-root.putChild(b"stylesheets", File("./www.godfield.net/stylesheets"))
-root.putChild(b"en.html", File("./www.godfield.net/en.html"))
-root.putChild(b"favicon.ico", File("./www.godfield.net/favicon.ico"))
-root.putChild(b"index.html", File("./www.godfield.net/index.html"))
-root.putChild(b"crossdomain.xml", File("./static.godfield.net/crossdomain.xml"))
-root.putChild(b"images", File("./static.godfield.net/images"))
-root.putChild(b"sounds", File("./static.godfield.net/sounds"))
+root.putChild(b"game", CORSFile("./www.godfield.net/game"))
+root.putChild(b"scripts", CORSFile("./www.godfield.net/scripts"))
+root.putChild(b"stylesheets", CORSFile("./www.godfield.net/stylesheets"))
+root.putChild(b"en.html", CORSFile("./www.godfield.net/en.html"))
+root.putChild(b"favicon.ico", CORSFile("./www.godfield.net/favicon.ico"))
+root.putChild(b"index.html", CORSFile("./www.godfield.net/index.html"))
+root.putChild(b"crossdomain.xml", CORSFile("./static.godfield.net/crossdomain.xml"))
+root.putChild(b"images", CORSFile("./static.godfield.net/images"))
+root.putChild(b"sounds", CORSFile("./static.godfield.net/sounds"))
 
 factory = Site(root)
 endpoint = endpoints.TCP4ServerEndpoint(reactor, 80)
